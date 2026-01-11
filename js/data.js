097b4823bbaf9d4710bb1156ed986034028c3f92
skills-lab-lgtm <Yernur.yertay.2007@mail.ru>
2026-01-11 16:55:08 +0500

    Create data.js

---
+++ js/data.js
@@ -0,0 +1,18 @@
+const objects = [
+  {
+    id: 1,
+    title: "3-комнатная квартира",
+    area: 100,
+    price: 69000000,
+    description: "Черновая отделка, высокий этаж",
+    image: "assets/images/flat1.jpg"
+  },
+  {
+    id: 2,
+    title: "2-комнатная квартира",
+    area: 62,
+    price: 32500000,
+    description: "Косметический ремонт, центр города",
+    image: "assets/images/flat2.jpg"
+  }
+];
